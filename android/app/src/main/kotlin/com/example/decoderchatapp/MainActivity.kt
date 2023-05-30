f5b18ee62fe69c06f476c5217d9dd086cb2d7314
Rahul <joinmerahul5714@gmail.com>
2023-05-30 13:57:24 +0530

    chatapp

---
+++ android/app/src/main/kotlin/com/example/decoderchatapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.decoderchatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:559653813679:web:bb6818c745201ac91ec35a";
+  static String apiKey = "AIzaSyDDKWJP4vnFffoDMqwwxUVA8FZjrpMYlP0";
+  static String messagingSenderId = "559653813679";
+  static String projectId = "decoder-schatapp";
+  final primaryColor = const Color(0xFFee7b64);
+}
